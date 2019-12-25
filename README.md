923e29aa36a9a9aff36187543f872d6879c1f0c2
zengniannian <457920700@qq.com>
2019-12-25 08:33:33 +0800

    add sql ddl

--- README.md
+++ README.md
@@ -6,9 +6,26 @@
 [Spring Web](https://spring.io/guides/gs/serving-web-content/)  
 [Bootstrap](https://v3.bootcss.com/components)  
 [GitHub OAuth](https://developer.github.com/apps/building-oauth-apps/creating-an-oauth-app/)  
-[OkHttp Document](https://square.github.io/okhttp/)
-
+[OkHttp Document](https://square.github.io/okhttp/)  
+[H2 Database Document](http://www.h2database.com/)  
+[mybatis-spring-boot](http://mybatis.org/spring-boot-starter/mybatis-spring-boot-autoconfigure/)  
+[Spring Document database](https://docs.spring.io/spring-boot/docs/current/reference/html/spring-boot-features.html#boot-features-embedded-database-support)
 
 ##工具
 [Git](https://github.com/)  
 [OAuth test](https://developer.github.com/apps/building-oauth-apps/authorizing-oauth-apps/)
+
+
+##脚本
+'''sql
+CREATE CACHED TABLE "PUBLIC"."USER"(
+    "ID" INT  NOT NULL NULL_TO_DEFAULT SEQUENCE "PUBLIC"."SYSTEM_SEQUENCE_2619B086_F183_44CA_9EEF_A3A4B1DE7B42",
+    "ACCOUNT_ID" VARCHAR(100),
+    "NAME" VARCHAR(50),
+    "TOKEN" CHAR(36) COMMENT STRINGDECODE('\u5f53\u524d\u8d26\u6237\u7684token\n'),
+    "GMT_CREATE" BIGINT COMMENT STRINGDECODE('\u521b\u5efa\u7684\u65f6\u95f4'),
+    "GMT_MODIFIED" BIGINT COMMENT STRINGDECODE('\u4fee\u6539\u4f4d')
+)
+
+
+'''
--- src/main/java/life/majiang/community/controller/AuthorizeController.java
+++ src/main/java/life/majiang/community/controller/AuthorizeController.java
@@ -3,6 +3,8 @@ package life.majiang.community.controller;
 import com.sun.org.apache.xerces.internal.impl.xs.util.XSInputSource;
 import life.majiang.community.dto.AccessTokenDTO;
 import life.majiang.community.dto.GithubUser;
+import life.majiang.community.mapper.UserMapper;
+import life.majiang.community.model.User;
 import life.majiang.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,6 +12,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
 
 /*
 * Authorized Controller
@@ -27,9 +32,13 @@ public class AuthorizeController {
     @Value("${github.redirect.uri}")
     private String redirectUri;
 
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code,
-                           @RequestParam(name="state") String state){
+                           @RequestParam(name="state") String state,
+                           HttpServletRequest request){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setCode(code);
@@ -38,8 +47,22 @@ public class AuthorizeController {
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         //第一时间打印用户的信息
-        GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        return "index";
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+        System.out.println(githubUser.getName());
+        if(githubUser != null){
+            //login success ,enter session or cookie
+            User user = new User();
+            user.setToken(UUID.randomUUID().toString());
+            user.setName(githubUser.getName());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
+            request.getSession().setAttribute("user",githubUser);
+            return "redirect:/";
+        }else{
+            //fail to login try again
+            return "redirect:/";
+        }
     }
 }
