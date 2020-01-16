cae3111412f6fe6f355e3734bd34b8c3be25e6b4
zengniannian <457920700@qq.com>
2020-01-16 10:53:19 +0800

    a new branch dev添加视频详情页面

---
+++ src/main/java/life/majiang/community/controller/VideosController.java
@@ -0,0 +1,25 @@
+package life.majiang.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class VideosController {
+
+    @GetMapping("/videos")
+    public String videos() {
+        return "videos";
+    }
+
+    @GetMapping("/video")
+    public String video(@RequestParam(name = "aid") String aid,
+                        @RequestParam(name = "page",defaultValue = "1") String page,
+                        Model model) {
+        model.addAttribute("aid", aid);
+        model.addAttribute("page", page);
+        return "video";
+    }
+}
